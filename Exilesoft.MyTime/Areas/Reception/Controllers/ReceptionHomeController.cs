5df5fc98219dc3c89c2c0b15fbba3e9ad94fe8a9
psn <psn@tiqri.com>
2022-05-18 13:00:06 +0530

    Added missing changes in Exilesoft.Mytime/Areas/Reception

--- Exilesoft.MyTime/Areas/Reception/Controllers/ReceptionHomeController.cs
+++ Exilesoft.MyTime/Areas/Reception/Controllers/ReceptionHomeController.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
 using System.Web.Security;
@@ -12,121 +13,137 @@ using Exilesoft.MyTime.Areas.Reception.Common;
 using Exilesoft.MyTime.Areas.Reception.Filters;
 using Exilesoft.MyTime.Areas.Reception.Repositories;
 using Exilesoft.MyTime.Helpers;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
 
 namespace Exilesoft.MyTime.Areas.Reception.Controllers
 {
 	public class ReceptionHomeController : Controller
 	{
-        public static string CookieName
-        {
-            get
-            {
-                return System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/Areas/Reception/Web.config")
-                        .AppSettings.Settings["ReceptionDesktopCoockieName"].Value;
-            }
-        }
+		public static string CookieName
+		{
+			get
+			{
+				return System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/Areas/Reception/Web.config")
+						.AppSettings.Settings["ReceptionDesktopCoockieName"].Value;
+			}
+		}
 
 		public ActionResult Index()
 		{
-			
 
-            var authCookie = System.Web.HttpContext.Current.Request.Cookies.Get(CookieName);
 
-            if (authCookie == null || string.IsNullOrEmpty(authCookie.Value))
-            {
-                return Redirect(ReceptionHelper.Authenticate(DeviceType.Desktop));
-            }
+			//var authCookie = System.Web.HttpContext.Current.Request.Cookies.Get(CookieName);
 
-            FormsAuthenticationTicket authenticationTicket = FormsAuthentication.Decrypt(authCookie.Value);
+			//if (authCookie == null || string.IsNullOrEmpty(authCookie.Value))
+			//{
+			//    return Redirect(ReceptionHelper.Authenticate(DeviceType.Desktop));
+			//}
 
-            if (authenticationTicket == null)
-            {
-                return Redirect(ReceptionHelper.Authenticate(DeviceType.Desktop));
-            }
+			//FormsAuthenticationTicket authenticationTicket = FormsAuthentication.Decrypt(authCookie.Value);
 
-            if (authenticationTicket.Expired)
-            {
-                return Redirect(ReceptionHelper.Authenticate(DeviceType.Desktop));
-            }
+			//if (authenticationTicket == null)
+			//{
+			//    return Redirect(ReceptionHelper.Authenticate(DeviceType.Desktop));
+			//}
 
-			return View();
+			//if (authenticationTicket.Expired)
+			//{
+			//    return Redirect(ReceptionHelper.Authenticate(DeviceType.Desktop));
+			//}
+			if (!Request.IsAuthenticated)
+			{
+				HttpContext.GetOwinContext().Authentication.Challenge(
+					new AuthenticationProperties { RedirectUri = "/" },
+					OpenIdConnectAuthenticationDefaults.AuthenticationType);
+			}
+			if (User.IsInRole("Receptionist"))
+			{
+				return View();
+			}
+			else
+			{
+				return Content("<html><p><i>Hello! You don't have permission to view this page.</u></i></p></html>", "text/html");
+			}
+
+
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult Dashboard()
 		{
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult History()
 		{
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult EmployeeSearch()
 		{
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult EmployeeDetails()
 		{
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult VisitorDetails(string deviceType)
 		{
 			ViewBag.deviceType = deviceType;
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult VisitorBasicDetails(string deviceType)
 		{
 			ViewBag.deviceType = deviceType;
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult VisitorAdvancedDetails(string deviceType)
 		{
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult VisitInformation()
 		{
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult UnderConstruction()
 		{
 			return View();
 		}
 
-        //[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
+		//[ReceptionAuthentication("Admin", "PMO", "Receptionist")]
 		public ActionResult ViewAllCardHistory()
 		{
 			return View();
 		}
 
-        public ActionResult CallBack(string code)
-        {
-            var data = new StringBuilder();
+		public ActionResult CallBack(string code)
+		{
+			var data = new StringBuilder();
 
-            data.Append("code=" + code);
-            data.Append("&clientId=" + Uri.EscapeDataString(ReceptionHelper.Base64Encode(ConfigurationManager.AppSettings["ClientId"])));
-            data.Append("&clientSecret=" + Uri.EscapeDataString(ReceptionHelper.Base64Encode(ConfigurationManager.AppSettings["ClientSecret"])));
-            data.Append("&redirectUri=" + "");
+			data.Append("code=" + code);
+			data.Append("&clientId=" + Uri.EscapeDataString(ReceptionHelper.Base64Encode(ConfigurationManager.AppSettings["ClientId"])));
+			data.Append("&clientSecret=" + Uri.EscapeDataString(ReceptionHelper.Base64Encode(ConfigurationManager.AppSettings["ClientSecret"])));
+			data.Append("&redirectUri=" + "");
 
-            ReceptionHelper.HttpPost(data, ConfigurationManager.AppSettings["TokenkUrl"], DeviceType.Desktop);
+			ReceptionHelper.HttpPost(data, ConfigurationManager.AppSettings["TokenkUrl"], DeviceType.Desktop);
 
-            return Redirect(ConfigurationManager.AppSettings["DefaultUrl"] + "/reception");
-        }
+			return Redirect(ConfigurationManager.AppSettings["DefaultUrl"] + "/reception");
+		}
 
 		public ActionResult LogOut()
 		{
--- Exilesoft.MyTime/Areas/Reception/Filters/ReceptionAuthentication.cs
+++ Exilesoft.MyTime/Areas/Reception/Filters/ReceptionAuthentication.cs
@@ -10,86 +10,86 @@ using Exilesoft.MyTime.Helpers;
 
 namespace Exilesoft.MyTime.Areas.Reception.Filters
 {
-    public class ReceptionAuthentication : AuthorizeAttribute
-    {
-        private readonly string[] _roleArray;
+  //  public class ReceptionAuthentication : AuthorizeAttribute
+  //  {
+  //      private readonly string[] _roleArray;
 
-        public static string CookieName
-        {
-            get
-            {
-                return System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/Areas/Reception/Web.config")
-                        .AppSettings.Settings["ReceptionDesktopCoockieName"].Value;
-            }
-        }
+  //      public static string CookieName
+  //      {
+  //          get
+  //          {
+  //              return System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/Areas/Reception/Web.config")
+  //                      .AppSettings.Settings["ReceptionDesktopCoockieName"].Value;
+  //          }
+  //      }
 
-        public ReceptionAuthentication(params string[] roles)
-		{
-			_roleArray = roles;
-		}
+  //      public ReceptionAuthentication(params string[] roles)
+		//{
+		//	_roleArray = roles;
+		//}
 
-		public override void OnAuthorization(AuthorizationContext filterContext)
-		{
-            var authCookie = HttpContext.Current.Request.Cookies.Get(CookieName);
+		//public override void OnAuthorization(AuthorizationContext filterContext)
+		//{
+  //          var authCookie = HttpContext.Current.Request.Cookies.Get(CookieName);
 
-            if (authCookie == null || string.IsNullOrEmpty(authCookie.Value))
-		    {
-                filterContext.Result = new RedirectResult("ReceptionHome/Index");
-		    }
-		    else
-		    {
-		        FormsAuthenticationTicket authenticationTicket = FormsAuthentication.Decrypt(authCookie.Value);
+  //          if (authCookie == null || string.IsNullOrEmpty(authCookie.Value))
+		//    {
+  //              filterContext.Result = new RedirectResult("ReceptionHome/Index");
+		//    }
+		//    else
+		//    {
+		//        FormsAuthenticationTicket authenticationTicket = FormsAuthentication.Decrypt(authCookie.Value);
 
-		        if (authenticationTicket == null)
-		        {
-		            HandleUnotherizeResponce(filterContext, HttpStatusCode.Unauthorized, "Invalid Token");
-                    filterContext.Result = new RedirectResult("ReceptionHome/Index");
-		            return;
-		        }
+		//        if (authenticationTicket == null)
+		//        {
+		//            HandleUnotherizeResponce(filterContext, HttpStatusCode.Unauthorized, "Invalid Token");
+  //                  filterContext.Result = new RedirectResult("ReceptionHome/Index");
+		//            return;
+		//        }
 
-		        if (authenticationTicket.Expired)
-		        {
-		            HandleUnotherizeResponce(filterContext, HttpStatusCode.Forbidden, "Invalid Expired");
-                    filterContext.Result = new RedirectResult("ReceptionHome/Index");
-		            return;
-		        }
+		//        if (authenticationTicket.Expired)
+		//        {
+		//            HandleUnotherizeResponce(filterContext, HttpStatusCode.Forbidden, "Invalid Expired");
+  //                  filterContext.Result = new RedirectResult("ReceptionHome/Index");
+		//            return;
+		//        }
 
-		        //user data format EmployeeId, Roles
-		        string[] userData = authenticationTicket.UserData.Split(',');
-		        string employeeId = userData[0];
-		        string[] roles = userData[1].Split('|');
-		        string userName = authenticationTicket.Name;
-                var userIdentity = new GenericIdentity(userName);
-		        var userPrincipal = new GenericPrincipal(userIdentity, roles);
-		        HttpContext.Current.User = userPrincipal;
+		//        //user data format EmployeeId, Roles
+		//        string[] userData = authenticationTicket.UserData.Split(',');
+		//        string employeeId = userData[0];
+		//        string[] roles = userData[1].Split('|');
+		//        string userName = authenticationTicket.Name;
+  //              var userIdentity = new GenericIdentity(userName);
+		//        var userPrincipal = new GenericPrincipal(userIdentity, roles);
+		//        HttpContext.Current.User = userPrincipal;
 
-		        HttpRuntime.Cache.Insert("EmployeeId", employeeId);
+		//        HttpRuntime.Cache.Insert("EmployeeId", employeeId);
 
-		        if (!IsUserInRoles())
-		        {
-                    filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                    filterContext.HttpContext.Response.StatusDescription = HttpStatusCode.Unauthorized.ToString();
-                    return;
-		        }
+		//        if (!IsUserInRoles())
+		//        {
+  //                  filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+  //                  filterContext.HttpContext.Response.StatusDescription = HttpStatusCode.Unauthorized.ToString();
+  //                  return;
+		//        }
 
-                CookieHelper.SetCookie(userName, authenticationTicket.UserData);
-		    }
-		}
+  //              CookieHelper.SetCookie(userName, authenticationTicket.UserData);
+		//    }
+		//}
 
-		private void HandleUnotherizeResponce(AuthorizationContext actionContext, HttpStatusCode status, string message)
-		{
-			actionContext.HttpContext.Response.StatusCode = (int) status;
-			actionContext.HttpContext.Response.StatusDescription = status.ToString(); 
-			HttpContext.Current.User = null;
-		}
+		//private void HandleUnotherizeResponce(AuthorizationContext actionContext, HttpStatusCode status, string message)
+		//{
+		//	actionContext.HttpContext.Response.StatusCode = (int) status;
+		//	actionContext.HttpContext.Response.StatusDescription = status.ToString(); 
+		//	HttpContext.Current.User = null;
+		//}
 
-		private bool IsUserInRoles()
-		{
-			if (_roleArray.Any())
-			{
-				return _roleArray.Any(role => HttpContext.Current.User.IsInRole(role));
-			}
-			return true;
-		}
-    }
+		//private bool IsUserInRoles()
+		//{
+		//	if (_roleArray.Any())
+		//	{
+		//		return _roleArray.Any(role => HttpContext.Current.User.IsInRole(role));
+		//	}
+		//	return true;
+		//}
+  //  }
 }
